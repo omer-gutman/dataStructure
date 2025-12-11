300d7c83148cbc23d773f719e49e48762941f8f4
guom2003@gmail.com <guom2003@gmail.com>
2025-12-11 09:24:20 +0200

    Initial commit

---
+++ MyDataStructure.java
@@ -0,0 +1,39 @@
+
+public class MyDataStructure {
+	/*
+     * You may add any fields that you wish to add.
+     * Remember that the use of built-in Java classes is not allowed,
+     * the only variables types you can use are: 
+     * 	-	the given classes in the assignment
+     * 	-	basic arrays
+     * 	-	primitive variables
+     */
+	
+	/***
+     * This function is the Init function.
+     */
+	public MyDataStructure() {
+		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+	}
+	
+	public void insert(int id, int quality) {
+		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+	}
+	
+	public void delete(int id) {
+		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+	}
+	
+	public int medianQuality() {
+		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+	}
+	
+	public double avgQuality() {
+		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+	}
+	
+	public MyLinkedList<Product> junkWorst() {
+		throw new UnsupportedOperationException("Delete this line and replace it with your implementation");
+	}
+	
+}
